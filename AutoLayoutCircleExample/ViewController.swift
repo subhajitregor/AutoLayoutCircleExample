1ebee08f443fecfa03baa9ef8fc4e98b101601e5
subhajitregor <subhajit.regor.official@gmail.com>
2017-05-16 11:27:12 +0530

    done

--- AutoLayoutCircleExample/ViewController.swift
+++ AutoLayoutCircleExample/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var midView: UIView!
+    @IBOutlet weak var botView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // initialCommit
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.layoutIfNeeded()
+        topView.layer.cornerRadius = topView.bounds.height / 2
+        midView.layer.cornerRadius = midView.bounds.height / 2
+        botView.layer.cornerRadius = botView.bounds.height / 2
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
